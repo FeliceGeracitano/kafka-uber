1a1009a1fe02a77f13499d31eec2933410717cf5
Felice <felice.geracitano@utmost.co>
2020-04-20 13:32:57 +0100

    refactor: 💡 remove axios

--- frontend/src/mapbox.js
+++ frontend/src/mapbox.js
@@ -1,20 +1,15 @@
-import axios from "axios";
-import mapbox from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import mapbox from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 mapbox.accessToken =
-  "pk.eyJ1IjoiZmVsaWNlZ2VyYWNpdGFubyIsImEiOiJjanhtMDJpMWYwMno2M29vNDJmbDZoZ2NjIn0.wi38HXu-VVmUkJkkqs3zVA";
+  'pk.eyJ1IjoiZmVsaWNlZ2VyYWNpdGFubyIsImEiOiJjanhtMDJpMWYwMno2M29vNDJmbDZoZ2NjIn0.wi38HXu-VVmUkJkkqs3zVA'
 
-const mapContextKey = "mapContextKey";
-export { mapbox, mapContextKey };
+const mapContextKey = 'mapContextKey'
+export { mapbox, mapContextKey }
 
 export async function getDirections(...locations) {
-  const coordinatesString = locations
-    .map(({ lon, lat }) => `${lon},${lat}`)
-    .join(";");
-  const endpoint = `
-  https://api.mapbox.com/directions/v5/mapbox/driving/${coordinatesString}?geometries=geojson&access_token=${mapbox.accessToken}
-  `;
-  const resp = await axios.get(endpoint);
-  return resp.data;
+  const coordinatesString = locations.map(({ lon, lat }) => `${lon},${lat}`).join(';')
+  const endpoint = `https://api.mapbox.com/directions/v5/mapbox/driving/${coordinatesString}?geometries=geojson&access_token=${mapbox.accessToken}`
+  const resp = await (await fetch(endpoint)).json()
+  return resp.data
 }
