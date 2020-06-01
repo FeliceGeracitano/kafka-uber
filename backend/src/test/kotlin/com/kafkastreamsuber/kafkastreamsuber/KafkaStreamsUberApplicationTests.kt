c6d333dbc813e598cb910b70a0a0ef7bbc96112e
Felice <felice.geracitano@utmost.co>
2020-06-01 22:49:27 +0100

    progress

--- backend/src/test/kotlin/com/kafkastreamsuber/kafkastreamsuber/KafkaStreamsUberApplicationTests.kt
+++ backend/src/test/kotlin/com/kafkastreamsuber/kafkastreamsuber/KafkaStreamsUberApplicationTests.kt
@@ -3,6 +3,8 @@ package com.kafkastreamsuber.kafkastreamsuber
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+
+// Triggy
 @SpringBootTest
 class KafkaStreamsUberApplicationTests {
 
